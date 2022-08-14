f4b8201680517231b03b957732901070a3d21988
Murasakinosora <joshuatolentino44444@gmail.com>
2022-08-14 23:25:01 +0800

    hotdogs

---
+++ Initials.py
@@ -0,0 +1,10 @@
+## initials of me 
+
+print("           J            T T T T T")
+print("           J                T")
+print("           J                T")
+print("           J                T")
+print("           J                T")
+print("           J                T")
+print("           J                T")
+print("   J J J J J                T")
---
+++ bmi.py
@@ -0,0 +1,6 @@
+mass = 159
+height = 1.6
+
+bmi = mass/(pow(height,2))
+
+print(bmi)
