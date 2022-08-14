dba16a6dade9273b96515fa7488187da09470842
Murasakinosora <joshuatolentino44444@gmail.com>
2022-08-14 23:29:36 +0800

    delete

--- bmi.py
+++
@@ -1,6 +0,0 @@
-mass = 159
-height = 1.6
-
-bmi = mass/(pow(height,2))
-
-print(bmi)
--- temperature.py
+++
@@ -1,6 +0,0 @@
-temp_inFahrenheit = 76
-
-temp = temp_inFahrenheit - 32
-convert_toCelcius = temp/1.8
-
-print(convert_toCelcius)
