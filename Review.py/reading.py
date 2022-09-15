7e82b59ad020b71e8d3ca517d97b1c34df1c02a7
Murasakinosora <joshuatolentino44444@gmail.com>
2022-09-16 01:38:58 +0800

    revision

--- Review.py/reading.py
+++ Review.py/reading.py
@@ -1,2 +1,9 @@
 #reading a file
-reader = open("samplefile.txt","r")
+reader = open("D:\Codedex\CodeDex\Review.py\samplefile.txt","r")
+# writing = w, reading and writing = r+, appending = a
+#reader.readable() checks if a file is readable
+#to read whole file: print(reader.read())
+#to read the per line: print(reader.readline())
+# to put them in an array per line use
+print(reader.readlines())
+reader.close
